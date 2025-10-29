89457ed4eabb99cb6a48a3640dab521e78eeff34
DadiaZ <diogo-uds@hotmail.com>
2025-10-29 14:20:15 +0000

    feat: added module 5

---
+++ Module 5/ex00/includes/Bureaucrat.hpp
@@ -0,0 +1,45 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+//COLORS:
+#define RESET "\033[0m"
+#define CLASS_CALL "\033[35m"
+#define TEST "\033[36m"
+#define SOUND "\033[33m"
+#define TITLE "\033[32m"
+
+#include <iostream>
+#include <string>
+
+class Bureaucrat {
+    private:
+        std::string name;
+        int grade;
+    public:
+        Bureaucrat();
+        Bureaucrat(const std::string &name, int grade);
+        Bureaucrat(const Bureaucrat& other);
+        Bureaucrat& operator=(const Bureaucrat& other);
+        ~Bureaucrat();
+
+        std::string getName() const;
+        int getGrade() const;
+
+        void incrementGrade();
+        void decrementGrade();
+
+        class GradeTooHighException : public std::exception {
+            public:
+                const char *what() const throw();
+        };
+
+        class GradeTooLowException : public std::exception {
+            public:
+                const char *what() const throw();
+        };
+
+};
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &bureaucrat);
+
+#endif
---
+++ Module 5/ex00/src/Bureaucrat.cpp
@@ -0,0 +1,60 @@
+#include "includes/Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : name("default"), grade(150) {
+    std::cout << CLASS_CALL << "[CLASS CALL] " << RESET << "Bureaucrat constructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const std::string &name, int grade): name(name), grade(grade) {
+    std::cout << CLASS_CALL << "[CLASS CALL] " << RESET << "Bureaucrat constructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other) {
+    std::cout << CLASS_CALL << "[CLASS CALL] " << RESET << "Bureaucrat Copy constructor called" << std::endl;
+    *this = other;
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other) {
+    std::cout << CLASS_CALL << "[CLASS CALL] " << RESET << "Bureaucrat copy assignment operator called" << std::endl;
+    if (this != &other)
+        this->name = other.name;
+        this->grade = other.grade;
+    return *this;
+}
+
+Bureaucrat::~Bureaucrat() {
+    std::cout << CLASS_CALL << "[CLASS CALL] " << RESET << "Bureaucrat destructor called" << std::endl;
+}
+
+std::string Bureaucrat::getName() const {
+    return this->name;
+}
+
+int Bureaucrat::getGrade() const {
+    return this->grade;
+}
+
+void Bureaucrat::incrementGrade() {
+    if (grade <= 1)
+        throw Bureaucrat::GradeTooHighException();
+    grade--;
+}
+
+void Bureaucrat::decrementGrade() {
+    if (grade >= 150)
+        throw Bureaucrat::GradeTooLowException();
+    grade++;
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw() {
+    return "Bureaucrat grade is too high. Only accept grades between 1-150";
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw() {
+    return "Bureaucrat grade is too low. Only accept grades between 1-150";
+}
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &bureaucrat)
+{
+    out << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade() << ".";
+    return out;
+}
